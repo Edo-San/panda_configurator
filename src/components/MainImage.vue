203f272e88668242ded19454893e904c5dd12426
Edo San <edo.sandon@gmail.com>
2020-06-20 13:14:25 +0200

    feat: set dynamic image based on color

--- src/components/MainImage.vue
+++ src/components/MainImage.vue
@@ -1,13 +1,21 @@
 <template>
   <div class="MainImage">
-    <img class="MainImage__image" src="@/assets/images/panda/arancione.png" />
+    <img
+      class="MainImage__image"
+      :src="require(`@/assets/images/panda/${getSelectedColor.code}.png`)"
+    />
   </div>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
+import { mapGetters } from "vuex";
+
 export default Vue.extend({
-  name: "MainImage"
+  name: "MainImage",
+  computed: {
+    ...mapGetters("colors", ["getSelectedColor"])
+  }
 });
 </script>
 
@@ -15,5 +23,9 @@ export default Vue.extend({
 .MainImage {
   width: 100%;
   text-align: center;
+
+  &__image {
+    width: 342px;
+  }
 }
 </style>
--- src/store/colors/getters.ts
+++ src/store/colors/getters.ts
@@ -1,3 +1,5 @@
 export default {
   getColors: (state) => state.colors,
+  getSelectedColor: (state) =>
+    state.colors.find((color) => color.isSelected === true)
 };
