0cb7cebd36878cd8ea9ce6855ee8622a73d6cfdf
EdoSan <edo.sandon@gmail.com>
2020-06-25 22:50:30 +0200

    feat: add stepOption general component

---
+++ src/components/StepOption.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="Option">
+    <div
+      class="Option__Swatch"
+      :class="[`RimOption__Swatch--${option.code}`]"
+      @click="setSelected(option)"
+    >
+      <span
+        class="material-icons CheckBoxIcon"
+        :class="[
+          option.code === 'black' && 'CheckBoxIcon--inverted',
+          !option.isSelected && 'CheckBoxIcon--off'
+        ]"
+        >check_box</span
+      >
+    </div>
+    <div class="Option__Text">
+      <h3 class="Option__Text__Title">{{ option.title }}</h3>
+      <p class="Option__Text__Description">{{ option.description }}</p>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  props: {
+    option: {
+      type: Object,
+      required: true
+    },
+    setSelected: {
+      type: Function,
+      required: true
+    }
+  }
+});
+</script>
+
+<style lang="scss" scoped>
+</style>
